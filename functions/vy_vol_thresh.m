1fa127d6912354503a988c9da98da44dd197e0eb
Vahab Youssof Zadeh <vyoussofzadeh@squiggles.rcc.mcw.edu>
2019-05-20 08:42:06 -0500

    remove duplicated directory

--- functions/vy_vol_thresh.m
+++
@@ -1,9 +0,0 @@
-function s_vol = vy_vol_thresh(s_vol,projthresh)
-
-
-% set threshold
-% projthresh = 0.5;
-val = s_vol.anatomy;
-val(abs(val) < projthresh*max(abs(val(:)))) = NaN;
-val = val./max(abs(val(:)));
-s_vol.anatomy = val;
